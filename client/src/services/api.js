bb443287ed3a710cbd095c8b5a4f2a4ca9c2ce14
wanleng <156537441+wanleng@users.noreply.github.com>
2025-03-24 03:49:56 +0700

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.VITE_URL,  // Make sure this matches your server URL
+    baseURL: import.meta.env.VITE_API_URL,  // Make sure this matches your server URL
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
