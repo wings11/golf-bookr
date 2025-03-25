ed2f2683e9e3e8890f304126ad729205cfce7184
wings11 <wykyaw2001@gmail.com>
2025-03-25 16:48:44 +0700

    g

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
